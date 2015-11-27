cdf38dff203d86c2c2a1880540e280bfee686af7
Rolando Islas <admin@rolandoislas.com>
2015-11-26 20:29:05 -0700

    Add more inputs

--- MATH/Gui/Calc/ProbabilityDistribution.cs
+++ MATH/Gui/Calc/ProbabilityDistribution.cs
@@ -11,23 +11,37 @@ namespace MATH.Gui.Calc {
         public ProbabilityDistribution() {
             this.Text = "Probability Distribution";
             this.addListInput("P(x)");
-            this.setListPlaceHolderText("x");
-            this.setListPlaceHolderText("P(x)", "P(x)");
+            this.addInputs(
+                "Mean",
+                "Standard Deviation",
+                "Z-Score"
+            );
             this.addOutputs(new string[] {
                 "Mean",
                 "Variance",
-                "Standard Deviation"
+                "Standard Deviation",
+                "Z-Score",
+                "P(x)"
             });
+            this.setListPlaceHolderText("x");
+            this.setListPlaceHolderText("P(x)", "P(x)");
         }
 
         internal override void doCalculations() {
             double[] xList = getListInput();
             double[] pxList = getListInput("P(x)");
             if (xList.Length == 0 || pxList.Length == 0 || xList.Length != pxList.Length)
-                return;
-            setOutput("Mean", MMath.probDistMean(xList, pxList));
+                if (!areFieldsPopulated("Mean", "Standard Deviation"))
+                    return;
+            double mean = isFieldPopulated("Mean") ? getInput("Mean") : MMath.probDistMean(xList, pxList);
+            double standardDeviation = isFieldPopulated("Standard Deviation") ? getInput("Standard Deviation") : MMath.probDistStdDev(xList, pxList);
+            setOutput("Mean", mean);
             setOutput("Variance", MMath.probDistVariance(xList, pxList));
-            setOutput("Standard Deviation", MMath.probDistStdDev(xList, pxList));
+            setOutput("Standard Deviation", standardDeviation);
+            if(pxList.Length == 1)
+                setOutput("Z-Score", (pxList.First() - mean) / standardDeviation);
+            if (isFieldPopulated("Z-Score"))
+                setOutput("P(x)", mean + getInput("Z-Score") * standardDeviation);
         }
 
     }
